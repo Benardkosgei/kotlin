cf3105625383a19b8e17b4ec70252dcec3ac9b19
Benard Kosgei <benardkosgei96@gmail.com>
2023-08-08 12:29:57 +0300

    Kotlin programs

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    //Mutable variables in kotlin
+    var num = 10
+    num = 56
+    println(num)
+    var name ="Kim"
+    // name = "kimtai" cannot be reassigned
+    println("My name is $name")
+}
